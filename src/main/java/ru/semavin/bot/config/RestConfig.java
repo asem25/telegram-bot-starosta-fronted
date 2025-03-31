f8f9741892d07791949bd3a5bfc46d59d87b1d90
asem25 <asemavin250604@gmail.com>
2025-03-31 19:57:15 +0300

    fix:bug with restclient

--- src/main/java/ru/semavin/bot/config/RestConfig.java
+++ src/main/java/ru/semavin/bot/config/RestConfig.java
@@ -6,10 +6,14 @@ import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplateConfig {
+public class RestConfig {
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+    @Bean
+    public RestClient restClient(){
+        return RestClient.builder().build();
     }
 }
