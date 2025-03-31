1782c17f91db7313c19379ec834cd3a8b11dc8f3
asem25 <asemavin250604@gmail.com>
2025-03-31 19:44:14 +0300

    add scheduling

--- src/main/java/ru/semavin/bot/config/RestTemplateConfig.java
+++ src/main/java/ru/semavin/bot/config/RestTemplateConfig.java
@@ -3,13 +3,13 @@ package ru.semavin.bot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestClientConfig {
+public class RestTemplateConfig {
 
     @Bean
-    public RestClient restClient() {
-        return RestClient.builder()
-                .build();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- src/main/java/ru/semavin/bot/service/ping/PingAPIService.java
+++ src/main/java/ru/semavin/bot/service/ping/PingAPIService.java
@@ -4,26 +4,33 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class PingAPIService {
-    private final RestClient restClient;
+    private final RestTemplate restTemplate;
     @Value("${api.url}")
     private String apiUrl;
     @Scheduled(fixedRate = 60000 * 3)
-    public void ping() {
-        log.info("Pinging API" );
-        HttpStatusCode response = restClient.get().uri(apiUrl + "/").retrieve().toBodilessEntity().getStatusCode();
-        if (response.is2xxSuccessful()) {
-            log.info("Pinging API OK");
-        }else {
-            log.info("Pinging API ERROR");
+    public void pingDailyApi() {
+        log.info("Telegram api ping starting");
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(apiUrl + "/", String.class);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                log.info("Telegram api ping finished success: {}", response.getBody());
+            }else{
+                log.error("Telegram api ping failed: {}", response.getBody());
+            }
+        } catch (RestClientException e) {
+            log.warn("Telegram api service error");
         }
     }
 
