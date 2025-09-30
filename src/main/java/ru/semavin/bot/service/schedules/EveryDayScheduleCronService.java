3490750e6c12d0d06e4f2e1becf0be2529f7ddb2
asem25 <asemavin250604@gmail.com>
2025-09-30 21:15:44 +0300

    SA-1-1 добавил фичи для ежеднового оповещения

--- src/main/java/ru/semavin/bot/service/schedules/EveryDayScheduleCronService.java
+++ src/main/java/ru/semavin/bot/service/schedules/EveryDayScheduleCronService.java
@@ -32,24 +32,52 @@ public class EveryDayScheduleCronService {
                     }
 
                     for (UserDTO student : students) {
-                        if (student.getTelegramId() != null) {
-                                    scheduleService.getForToday(student.getGroupName())
-                                    .thenCompose(schedule ->
-                                       messageSenderService.sendButtonMessage(
-                                            SendMessage.builder()
-                                                    .chatId(student.getTelegramId())
-                                                    .text(buildSchedule(schedule))
-                                                    .replyMarkup(KeyboardUtils.createMarkupWithTomorrow(LocalDate.now()))
-                                                    .build())
-                                    )
-                                    .exceptionally(e -> {
-                                        log.error("Ошибка при получении расписания на сегодня: {}", e.getMessage());
-                                        return null;
-                                    })
-                                    .thenApply(messageId -> {
-                                        log.debug("Команда 'Сегодня' выполнена для пользователя {}", student.getTelegramId());
-                                        return null;
-                                    });
+                        if (student.getTelegramId() != null & student.getUsername().equalsIgnoreCase("asem250604")) {
+
+                                scheduleService.getForToday(student.getGroupName())
+                                        .thenCompose(schedule -> {
+                                                    if (LocalDate.now().getDayOfWeek() == DayOfWeek.THURSDAY) {
+                                                        return messageSenderService.sendButtonMessage(
+                                                                SendMessage.builder()
+                                                                        .chatId(student.getTelegramId())
+                                                                        .text(buildMilitarySchedule(schedule))
+                                                                        .replyMarkup(KeyboardUtils.createMarkupWithTomorrow(LocalDate.now()))
+                                                                        .build());
+
+                                                    } else if (LocalDate.now().getDayOfWeek() == DayOfWeek.THURSDAY) {
+                                                        return messageSenderService.sendButtonMessage(
+                                                                SendMessage.builder()
+                                                                        .chatId(student.getTelegramId())
+                                                                        .text(buildHolidaySchedule(schedule))
+                                                                        .replyMarkup(KeyboardUtils.createMarkupWithTomorrow(LocalDate.now()))
+                                                                        .build());
+
+                                                    } else if (LocalDate.now().getDayOfWeek() == DayOfWeek.SATURDAY) {
+                                                        return messageSenderService.sendButtonMessage(
+                                                                SendMessage.builder()
+                                                                        .chatId(student.getTelegramId())
+                                                                        .text(buildSaturdaySchedule(schedule))
+                                                                        .replyMarkup(KeyboardUtils.createMarkupWithTomorrow(LocalDate.now()))
+                                                                        .build());
+                                                    }
+                                                    return messageSenderService.sendButtonMessage(
+                                                            SendMessage.builder()
+                                                                    .chatId(student.getTelegramId())
+                                                                    .text(buildSchedule(schedule))
+                                                                    .replyMarkup(KeyboardUtils.createMarkupWithTomorrow(LocalDate.now()))
+                                                                    .build());
+
+                                                }
+                                        )
+                                        .exceptionally(e -> {
+                                            log.error("Ошибка при получении расписания на сегодня: {}", e.getMessage());
+                                            return null;
+                                        })
+                                        .thenApply(messageId -> {
+                                            log.debug("Команда 'Сегодня' выполнена для пользователя {}", student.getTelegramId());
+                                            return null;
+                                        });
+
                         }
                     }
                     log.debug("Расписание отправлено студентам группы {}", defaultGroup);
@@ -61,6 +89,18 @@ public class EveryDayScheduleCronService {
     }
 
     private String buildSchedule(String schedule) {
-        return String.format("Доброе утро!\uD83C\uDF04 Сегодня в повестке дня \uD83D\uDD56: \n\n %s Хорошего дня!☺\uFE0F", schedule);
+        return String.format("Доброе утро!\uD83C\uDF04 Сегодня на повестке дня \uD83D\uDD56: \n\n %s Хорошего дня!☺\uFE0F", schedule);
+    }
+
+    private String buildMilitarySchedule(String schedule) {
+        return String.format("Военным доброе утро☀\uFE0F☀\uFE0F☀\uFE0F\nОстальным сладких снов!\n%s\n Продуктивного дня!", schedule);
+    }
+
+    private String buildHolidaySchedule(String schedule) {
+        return String.format("Доброе утро☀\uFE0F☀\uFE0F☀\uFE0F!\n%sПродуктивного дня! \nКста завтра выходные, сегодня по пивку??\uD83D\uDE0F" , schedule);
+    }
+
+    private String buildSaturdaySchedule(String schedule) {
+        return String.format("Доброе утро☀\uFE0F☀\uFE0F☀\uFE0F\nСегодня фигня, чисто развеяться \n%sПродуктивного дня!\uD83C\uDFD6\uFE0F\nСегодня опять по пивку??\uD83D\uDE0F" , schedule);
     }
 }
